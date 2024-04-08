f1dff585f96e6e86886236290321c518570aa089
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-08 18:37:41 +0000

    Corrected Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,10 @@ export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
 
-  /* vars are of the same value */
   if (trueOrFalse) {
     var task = false;
     var task2 = true;
   }
-  /*vars */
 
   return [task, task2];
 }
