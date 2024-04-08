4b7cce5c5e7903461f3c5976f1fb535dfe7a8b4e
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-08 20:16:27 +0000

    Task 1 and 4

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,12 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
+  /* disable no-unused-vars-eslint */
   if (trueOrFalse) {
     task = false;
     task2 = true;
   }
+  /* enable no-unused-vars-eslint */
 
   return [task, task2];
 }
