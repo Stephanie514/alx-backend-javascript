2eae41f1266e1bec69072aec1a8a728e40e51158
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-10 19:54:10 +0000

    Task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+/* Appending three handlers to the function */
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
+}
