f8ac29baad8fd1a3217451a9305345aaae18202a
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-11 20:43:11 +0000

    Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,14 +1,11 @@
-/* eslint-disable */
-import Car from './10-car';
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-export default class EVCar extends Car {
-    constructor(brand, motor, color, range) {
-        super(brand, motor, color);
-        this._range = range;
-    }
-
-    cloneCar() {
-        /* Create a new instance of Car with the same attributes as the current instance */
-        return new Car(this._brand, this._motor, this._color);
-    }
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
 }
