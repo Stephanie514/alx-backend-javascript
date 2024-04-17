a49c06d65b28359919a359b0a3e3779f2c30e96e
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-17 17:42:36 +0000

    Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,3 +20,19 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Defining the Directors interface that extends Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Testinhg the interface
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
