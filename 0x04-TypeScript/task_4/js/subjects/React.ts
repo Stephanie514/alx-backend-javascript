c8bf317686f0e291135bd7f0a5ce2735b7868234
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-20 16:09:48 +0000

    Namespace & Declaration merging

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,18 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  export class React extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for React";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingReact) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
+    }
+  }
+}
