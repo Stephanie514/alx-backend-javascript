65fa6d4368a20e972271d4395f50fe06945ee8ad
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-06-04 17:48:30 +0000

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,17 @@
-//program on 1-stdin
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-
-process.stdin.setEncoding('utf8');
+//program named 1-stdin
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    if (name) {
-        console.log(`Your name is: ${name}`);
-        process.exit();
-    }
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  if (process.stdin.isTTY) {
+    process.stdin.end();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.stdin.end();
+  }
 });
 
-process.on('exit', () => {
-    process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.exit(0);
 });
