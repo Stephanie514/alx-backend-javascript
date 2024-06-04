aec6600b9b6ceea29d58f45014f0bc77634d29e6
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-06-04 17:15:37 +0000

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,7 @@
 
 // Function to display message in standard output
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
 
-// Call function to display the message
-displayMessage("Hello, World!");
+module.exports = displayMessage;
