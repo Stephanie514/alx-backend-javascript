9a857dd7bbad81642a2219fa7643ffe303eb2aac
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-06-04 19:38:37 +0000

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,12 @@
+// 4-http.js
+
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-
   res.end('Hello Holberton School!\n');
 });
 
-server.listen(1245);
+app.listen(1245);
 
-module.exports = server;
+module.exports = app;
