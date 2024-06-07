0c672076edbb67885813f2a9c327af30904ab589
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-06-07 20:52:52 +0000

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,19 +3,17 @@ const http = require('http');
 const serverPort = 1245;
 const serverHost = 'localhost';
 
-const server = http.createServer();
-
-server.on('request', (_, res) => {
-  const respText = 'Hello Ho;berton School!';
+const server = http.createServer((req, res) => {
+  const respText = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', respText.length); // Length of "Hello Holberton School!"
+  res.setHeader('Content-Length', Buffer.byteLength(respText)); // Correctly calculated byte length
   res.statusCode = 200;
-  res.write(Buffer.from(respText));
+  res.end(respText); // response is properly ended
 });
 
 server.listen(serverPort, serverHost, () => {
-  process.stdout.write(`Server listening at -> http://${serverHost}:${serverPort}`);
+  process.stdout.write(`Server listening at -> http://${serverHost}:${serverPort}\n`);
 });
 
 module.exports = server;
