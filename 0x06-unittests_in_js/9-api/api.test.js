a43d849518a569503f64c8323f7d2f6b0b140b36
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-06-07 19:34:01 +0000

    Regex integration testing mandatory

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,18 +1,34 @@
-const request = require('request');
+const axios = require('axios');
 const { expect } = require('chai');
 
-describe('Cart page', () => {
-  it('Correct status code when :id is a number', (done) => {
-    request.get('http://localhost:7865/cart/12', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('GET / returns correct response', async () => {
+    const response = await axios.get(`${API_URL}/`);
+    expect(response.status).to.equal(200);
+    expect(response.data).to.equal('Welcome to the payment system');
   });
 
-  it('Correct status code when :id is NOT a number (=> 404)', (done) => {
-    request.get('http://localhost:7865/cart/hello', (error, response, body) => {
-      expect(response.statusCode).to.equal(404);
-      done();
-    });
+  it('GET /cart/:id returns correct response for valid :id', async () => {
+    const response = await axios.get(`${API_URL}/cart/47`);
+    expect(response.status).to.equal(200);
+    expect(response.data).to.equal('Payment methods for cart 47');
+  });
+
+  it('GET /cart/:id returns 404 response for negative number values in :id', async () => {
+    try {
+      await axios.get(`${API_URL}/cart/-47`);
+    } catch (error) {
+      expect(error.response.status).to.equal(404);
+    }
+  });
+
+  it('GET /cart/:id returns 404 response for non-numeric values in :id', async () => {
+    try {
+      await axios.get(`${API_URL}/cart/d200-44a5-9de6`);
+    } catch (error) {
+      expect(error.response.status).to.equal(404);
+    }
   });
 });
